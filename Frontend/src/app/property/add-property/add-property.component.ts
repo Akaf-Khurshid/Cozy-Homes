2651703c9158a61827f45e1408fa09b1428d6398
Akaf Khurshid <akaf13@gmail.com>
2023-01-19 14:31:46 -0500

    restore

---
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-property',
+  templateUrl: './add-property.component.html',
+  styleUrls: ['./add-property.component.css']
+})
+export class AddPropertyComponent implements OnInit {
+  @ViewChild('Form') addPropertyForm: NgForm;
+  SellRent = '1';
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    // this.addPropertyForm.controls['Name'].setValue('Default Value');
+
+    setTimeout(() => {
+      this.addPropertyForm.controls['Name'].setValue('Default Value');
+    });
+
+  }
+
+  onBack() {
+    this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    console.log('Congrats, form Submitted');
+    console.log(this.addPropertyForm);
+  }
+
+}
