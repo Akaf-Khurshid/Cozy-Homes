20d1e92180feaf0e808dff55cd9491141e223e01
Akaf Khurshid <akaf13@gmail.com>
2023-01-24 12:30:22 -0500

    Added WebAPI

---
+++ WebAPI/Controllers/CityController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new[] { "Atlanta", "New York" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return  "Atlanta";
+        }
+    }
+}
