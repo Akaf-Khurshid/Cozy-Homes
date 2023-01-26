c5c49b8a1e3c94d1a8bf202167ba994bee2632cc
Akaf Khurshid <akaf13@gmail.com>
2023-01-26 14:30:53 -0500

    Added repo

--- Frontend/src/app/property/add-property/add-property.component.ts
+++ Frontend/src/app/property/add-property/add-property.component.ts
@@ -26,7 +26,7 @@ export class AddPropertyComponent implements OnInit {
 
   propertyTypes: Array<string> = ['House', 'Apartment', 'Duplex'];
   furnishTypes: Array<string> = ['Fully', 'Semi', 'Unfurnished'];
-  cityList: string[];
+  cityList: any[];
 
   nextClicked: boolean;
   property = new Property();
--- WebAPI/Controllers/CityController.cs
+++ WebAPI/Controllers/CityController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Interfaces;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -7,10 +8,34 @@ namespace WebAPI.Controllers
     [ApiController]
     public class CityController : ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly ICityRepository repo;
+
+        public CityController(ICityRepository repo) 
         {
-            return new[] { "Atlanta", "New York" };
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCities()
+        {
+            var cities = await repo.GetCitiesAsync();
+            return Ok(cities);
+        }
+
+        [HttpPost("post")]
+        public async Task<IActionResult> AddCity(City city)
+        {
+            repo.AddCity(city);
+            await repo.SaveAsync();
+            return StatusCode(201);
+        }
+
+        [HttpDelete("delete/{id}")]
+        public async Task<IActionResult> DeleteCity(int id)
+        {
+            repo.DeleteCity(id);
+            await repo.SaveAsync();
+            return Ok(id);
         }
 
         [HttpGet("{id}")]
