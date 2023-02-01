b0b450e6b852da551def4fc86ab91d44989b665e
Akaf Khurshid <akaf13@gmail.com>
2023-02-01 09:50:18 -0500

    if to switch for readability in retryRequest

--- Frontend/src/app/services/httperor-interceptor.service.ts
+++ Frontend/src/app/services/httperor-interceptor.service.ts
@@ -1,18 +1,31 @@
-import { HttpErrorResponse, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { catchError, concatMap, Observable, of, retry, retryWhen, throwError } from "rxjs";
-import { AlertifyService } from "./alertify.service";
+import {
+  HttpErrorResponse,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  catchError,
+  concatMap,
+  Observable,
+  of,
+  retry,
+  retryWhen,
+  throwError,
+} from 'rxjs';
+import { ErrorCode } from '../enums/enums';
+import { AlertifyService } from './alertify.service';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class HttpErrorInterceptorService implements HttpInterceptor{
-  constructor(private alertifyService: AlertifyService){}
-  intercept(request: HttpRequest<any>, next: HttpHandler){
-    console.log("HTTP request started")
-    return next.handle(request)
-    .pipe(
-      retryWhen(error => this.retryRequest(error,10)),
-      catchError((error: HttpErrorResponse) =>{
+export class HttpErrorInterceptorService implements HttpInterceptor {
+  constructor(private alertifyService: AlertifyService) {}
+  intercept(request: HttpRequest<any>, next: HttpHandler) {
+    console.log('HTTP request started');
+    return next.handle(request).pipe(
+      retryWhen((error) => this.retryRequest(error, 10)),
+      catchError((error: HttpErrorResponse) => {
         const errorMessage = this.setError(error);
         console.log(error);
         this.alertifyService.error(errorMessage);
@@ -21,24 +34,31 @@ export class HttpErrorInterceptorService implements HttpInterceptor{
     );
   }
 
-  retryRequest(error: Observable<HttpErrorResponse>, retryCount: number) : Observable<unknown>
-  {
+  retryRequest(
+    error: Observable<HttpErrorResponse>,
+    retryCount: number
+  ): Observable<unknown> {
     return error.pipe(
       concatMap((chechErr: HttpErrorResponse, count: number) => {
-        if(chechErr.status === 0 && count <= retryCount){
-          return of(chechErr);
+        if (count <= retryCount) {
+          switch (chechErr.status) {
+            case ErrorCode.serverDown:
+              return of(chechErr);
+            // case ErrorCode.unauthorised:
+            //   return of(chechErr);
+          }
         }
         return throwError(chechErr);
       })
-    )
+    );
   }
 
   setError(error: HttpErrorResponse): string {
     let errorMessage = 'Unknown error occured';
-    if(error.error instanceof ErrorEvent){
+    if (error.error instanceof ErrorEvent) {
       errorMessage = error.error.message;
-    }else{
-      if(error.status != 0){
+    } else {
+      if (error.status != 0) {
         errorMessage = error.error.errorMessage;
       }
     }
